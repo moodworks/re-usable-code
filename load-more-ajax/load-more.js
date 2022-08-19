d76ae6dcd90bb93f0b78d49b3280eba7cd6119f2
moodworks <76935290+moodworks@users.noreply.github.com>
2022-08-19 21:03:10 +0200

    Create load-more.js

---
+++ load-more-ajax/load-more.js
@@ -0,0 +1,21 @@
+jQuery( document ).ready( function(){
+    var page = 2;
+    jQuery( function($) {
+      jQuery( 'body' ).on( 'click', '.loadmore', function() {
+        var data = {
+          'action': 'load_posts_by_ajax',
+          'page': page,
+          'security': blog.security
+        };
+        jQuery.post( blog.ajaxurl, data, function( response ) {
+          if( $.trim(response) != '' ) {
+            jQuery( '.blog-posts' ).append( response );
+            page++;
+          } else {
+            jQuery( '.loadmore' ).hide();
+            jQuery( ".no-more-post" ).html( "No More Post Available" );
+          }
+        });
+      });
+    });
+  });
